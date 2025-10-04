297ce7c4c0529d9d83c6d254dace1e9324f76008
arharuty <13937@coderacademy.edu.au>
2025-10-04 14:17:06 +1000

    implemented new sections in home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,17 @@
 import {Hero} from "../components/Hero.tsx";
 import {Statistics} from "../components/Statistics.tsx";
+import {SignUp} from "../components/SignUp.tsx";
+import {Introducing} from "../components/Introducing.tsx";
+import {LifetimeValue} from "../components/LifetimeValue.tsx";
 
 export const Home = () => {
     return (
         <div className="page">
             <Hero />
             <Statistics />
+            <SignUp />
+            <Introducing />
+            <LifetimeValue />
         </div>
     );
 };
