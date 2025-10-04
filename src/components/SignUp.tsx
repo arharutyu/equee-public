862a0975167902604fcec76b8d73e53bc9a88227
arharuty <13937@coderacademy.edu.au>
2025-10-04 14:13:29 +1000

    added signup section

---
+++ src/components/SignUp.tsx
@@ -0,0 +1,56 @@
+import './SignUp.css';
+
+export const SignUp = () => {
+    return (
+        <section className="signup section">
+            <div className="container">
+                <div className="signup-content">
+                    <div className="signup-text">
+                        <h2 className="signup-title">
+                            Imagine if there was a way to squeeze a financial adviser into your phone.
+                        </h2>
+
+                        <p className="signup-description">
+                            As financial advisers we know you don't want to spend a fortune making sure your super and life insurance is done right.
+                        </p>
+
+                        <p className="signup-description">
+                            So we got to work on creating a solution and we started with the idea of how we could put a financial adviser in your pocket.
+                        </p>
+
+                        <p className="signup-description">
+                            Specifically in your phone, but then we thought why not your laptop and desktop as well.*
+                        </p>
+
+                        <p className="signup-features-title">
+                            A simple site that allows you to:
+                        </p>
+
+                        <ul className="signup-features-list">
+                            <li className="signup-feature-item">
+                                Make sure your super is correctly invested by a licensed financial adviser
+                            </li>
+                            <li className="signup-feature-item">
+                                Make sure you have the insurance you need to protect you from life's ups and down
+                            </li>
+                        </ul>
+
+                        <div className="signup-actions">
+                            <button className="btn btn-secondary hover-lift hover-shadow">
+                                Sign up now
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className="signup-visual">
+                        <img
+                            src="/img/home-img-bridge.jpg"
+                            alt="Illustrated bridge representing the connection between you and financial advice"
+                            className="signup-illustration"
+                        />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
